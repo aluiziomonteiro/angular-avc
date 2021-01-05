b9acbaf2d1ec2bbb7fdb4e0958192fbf292c45ab
Aluizio <devaluizio@gmail.com>
2021-01-05 07:17:26 -0300

    knowing-project

--- curso-angular/src/app/app.routing.module.ts
+++ curso-angular/src/app/app.routing.module.ts
@@ -6,19 +6,25 @@ import { ListagemFilmesComponent } from './filmes/listagem-filmes/listagem-filme
 
 const routes: Routes = [
 
-  {
+  { // Rotas vazias serão redirecionadas para a rota filmes
       path: '',
       redirectTo: 'filmes',
       pathMatch: 'full'
   },
   {
+    // Rota filmes
     path: 'filmes',
+
+    // Filhos da rota filmes - Rotas que estão dentro de filmes
+    // Ex: localhost:4200/filmes/abc - O "abc" é filho de "/filmes"
     children: [
       {
+        // Digitou: localhost:4200/filmes - Vai cair no componente que lista os filmes
         path: '',
         component: ListagemFilmesComponent
       },
       {
+         // Digitou: localhost:4200/filmes/cadastro - Vai cair no componente que cadastra os filmes
         path: 'cadastro',
         component: CadastroFilmesComponent,
         pathMatch: 'full'
